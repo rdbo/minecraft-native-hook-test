2bad8dc725bae9304cdc8091030253ecd42aebe8
rdbo <rdbodev@gmail.com>
2024-08-26 08:19:05 -0300

    hook Minecraft::tick

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,25 @@ void JNICALL hk_hurtTo(JNIEnv *env, jobject obj, jfloat f)
 	std::cout << "[MCH] LocalPlayer::hurtTo(" << f << ") called!" << std::endl;
 }
 
+jclass orig_Minecraft_class;
+jmethodID orig_Minecraft_tick;
+void JNICALL hk_tick(JNIEnv *env, jobject obj)
+{
+	static unsigned long long tick_count = 0;
+	if (tick_count % 1000 == 0)
+		std::cout << "[MCH] Minecraft::tick() -> ticks: " << tick_count << std::endl;
+
+	// Call original Minecraft::tick()
+	env->CallNonvirtualVoidMethod(obj, orig_Minecraft_class, orig_Minecraft_tick);
+}
+
 void *thread_main(void *)
 {
 	JNIEnv *env;
 	JavaVM *jvm;
 	jnihook_t jnihook;
 
-	freopen("/tmp/mchook.log", "w", stdout);
+	// freopen("/tmp/mchook.log", "w", stdout);
 
 	std::cout << "[MCH] Library loaded!" << std::endl;
 
@@ -34,13 +46,25 @@ void *thread_main(void *)
 	auto LocalPlayer_class = env->FindClass("geb");
 	std::cout << "[MCH] LocalPlayer class: " << LocalPlayer_class << std::endl;
 
+	auto Minecraft_class = env->FindClass("fgo");
+	std::cout << "[MCH] Minecraft class: " << Minecraft_class << std::endl;
+
 	jmethodID hurtTo_method = env->GetMethodID(LocalPlayer_class, "H", "(F)V");
-	std::cout << "[MCH] LocalPlayer::hurtTo: " << hurtTo_method << std::endl;
+	std::cout << "[MCH] LocalPlayer::hurtTo(): " << hurtTo_method << std::endl;
+
+	jmethodID tick_method = env->GetMethodID(Minecraft_class, "t", "()V");
+	std::cout << "[MCH] Minecraft::tick():" << tick_method << std::endl;
 
 	std::cout << "[MCH] JNIHook_Init result: " << JNIHook_Init(env, &jnihook) << std::endl;
 
 	std::cout << "[MCH] JNIHook_Attach result: " <<
 		JNIHook_Attach(&jnihook, hurtTo_method, (void *)hk_hurtTo, NULL) << std::endl;
+
+	std::cout << "[MCH] JNIHook_Attach result: " <<
+		JNIHook_Attach(&jnihook, tick_method, (void *)hk_tick, &orig_Minecraft_class) << std::endl;
+
+	orig_Minecraft_tick = env->GetMethodID(orig_Minecraft_class, "t", "()V");
+	std::cout << "[MCH] Original Minecraft::tick(): " << orig_Minecraft_tick << std::endl;
 
 	jvm->DetachCurrentThread();
 
